e2c4feddf3f6e862e1646abd44e73eb9586dbd9a
Alecu <alecugajos1@gmail.com>
2025-05-31 14:50:49 +0200

    com

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogRoutes = require('./routes/blogRoutes')
 
 const app = express()
 
@@ -13,62 +13,15 @@ mongoose.connect(dbUrl)
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.redirect('/blogs')
 })
 
 
+app.use('/blogs/', blogRoutes)
 
-app.get('/blogs', (req, res) => {
-
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', { title: 'Home', blogs: result })
-        })
-        .catch((err) => { console.log(err) })
-})
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'New Blog' })
-})
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body)
-
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs')
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id
-
-    Blog.findById(id)
-        .then((response) => {
-            res.render('details', { blog: response , title: 'Deteils'})
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id
-
-    Blog.findByIdAndDelete(id)
-        .then((response) => {
-            res.json({ redirect: '/blogs'})
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' })
