b4b957926fa070ddf1370da9650ce60ae7e693aa
Alecu <alecugajos1@gmail.com>
2025-05-31 13:16:14 +0200

    com2

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.get('/', (req, res) => {
     res.redirect('/blogs')
 })
 
+
+
 app.get('/blogs', (req, res) => {
 
     Blog.find().sort({ createdAt: -1 })
@@ -27,12 +29,49 @@ app.get('/blogs', (req, res) => {
         .catch((err) => { console.log(err) })
 })
 
-app.get('/about', (req, res) => {
-    res.render('about', { title: 'About' })
-})
-
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'New Blog' })
+})
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body)
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id
+
+    Blog.findById(id)
+        .then((response) => {
+            res.render('details', { blog: response , title: 'Deteils'})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id
+
+    Blog.findByIdAndDelete(id)
+        .then((response) => {
+            res.json({ redirect: '/blogs'})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' })
 })
 
 app.use((req, res) => {
